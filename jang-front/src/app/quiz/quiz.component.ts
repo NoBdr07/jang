6f768ef0ce1731421c779192fc6c0b3adf7e40a4
Noémie <baudry.ddf@gmail.com>
2025-05-14 18:26:50 +0200

    feat: couleur et titre ok

--- jang-front/src/app/quiz/quiz.component.ts
+++ jang-front/src/app/quiz/quiz.component.ts
@@ -11,24 +11,34 @@ import { Page } from '../models/page.model';
 import { QuestionService } from '../services/question-service';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { FilterComponent } from "../filter/filter.component";
+import { FilterComponent } from '../filter/filter.component';
 import { MatCardModule } from '@angular/material/card';
+import { QuestionCardComponent } from '../question-card/question-card.component';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, FilterComponent, MatCardModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    FilterComponent,
+    QuestionCardComponent,
+    MatCardModule,
+  ],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss',
 })
 export class QuizComponent {
   // Nombre max de questions chargées
-  private maxQuestions = 20 ;
+  private maxQuestions = 20;
 
   // Streams de critère
   private niveau$ = new BehaviorSubject<number[]>([]);
   private topics$ = new BehaviorSubject<string[]>([]);
   private page$ = new BehaviorSubject<number>(0);
+
+  // Index de la question dans la page (0 à maxQuestions-1)
+  private index$  = new BehaviorSubject<number>(0);
 
   // Stream principal : dès qu'un critère change, on relance la requête
   questionsPage$: Observable<Page<QuestionDTO>> = combineLatest([
@@ -37,26 +47,46 @@ export class QuizComponent {
     this.page$,
   ]).pipe(
     switchMap(([niveaux, topics, page]) =>
-      this.questionService.getFilteredQuestions(niveaux, topics, page, this.maxQuestions)
+      this.questionService.getFilteredQuestions(
+        niveaux,
+        topics,
+        page,
+        this.maxQuestions
+      )
     )
   );
 
   // On en déduit la question courante
   currentQuestion$: Observable<QuestionDTO | undefined> =
-    this.questionsPage$.pipe(map((page) => page.content[0]));
+    combineLatest([this.questionsPage$, this.index$])
+    .pipe(
+      map(([page, idx]) => page.content[idx] )
+    );
 
   constructor(private questionService: QuestionService) {}
 
   // Méthodes appelées par le composant enfant filter
-  onFilterChange(filter: {niveau: number[]; topics: string[]}) {
+  onFilterChange(filter: { niveau: number[]; topics: string[] }) {
+    console.log("Appel de onFilterChange dans quiz component");
     this.niveau$.next(filter.niveau);
     this.topics$.next(filter.topics);
     this.page$.next(0);
+    this.index$.next(0);
   }
 
-  nextQuestion() {
+  nextPage() {
     const nextPage = this.page$.value + 1;
     this.page$.next(nextPage);
   }
 
+  nextQuestion() {
+    const idx = this.index$.value;
+    if( idx < this.maxQuestions) {
+      this.index$.next(idx + 1);
+    } else {
+      this.page$.next(this.page$.value + 1);
+      this.index$.next(0);
+    }
+    
+  }
 }
