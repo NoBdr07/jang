53c89ffb34620d3865867ccdbde00dae49b13bb4
Noémie <baudry.ddf@gmail.com>
2025-07-02 18:38:23 +0200

    front: remise en forme formulaire

--- jang-front/src/app/core/auth/register/register.component.ts
+++ jang-front/src/app/core/auth/register/register.component.ts
@@ -13,43 +13,45 @@ import { RegisterRequest } from '../../../shared/payload/register-request.interf
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
   private fb = inject(FormBuilder);
-    private router = inject(Router);
-    private authService = inject(AuthService);
-    private notificationService = inject(NotificationService);
-  
-    currentUser$ = this.authService.currentUser$;
-    private sub = new Subscription;
-  
-    form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    })
-  
-    ngOnInit(): void {
-       
-    }
-  
-    ngOnDestroy(): void {
-        this.sub.unsubscribe();
-    }
-  
-    onSubmit(): void {
-      if(this.form.valid) {
-        const req = this.form.value as RegisterRequest;
-        this.authService.register(req).subscribe({
-          error: () => {
-            this.notificationService.error("Le register n'a pas fonctionné.");
-          }
-        })
-      }
-    }
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private notificationService = inject(NotificationService);
 
+  currentUser$ = this.authService.currentUser$;
+  private sub = new Subscription();
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const req = this.form.value as RegisterRequest;
+      this.authService.register(req).subscribe({
+        error: () => {
+          this.notificationService.error("Le register n'a pas fonctionné.");
+        },
+      });
+    }
+  }
 }
